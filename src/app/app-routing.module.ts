55a6e08420623ad4257256013d7446e0bd006793
Anina Mahat <anin0543@edu.easj.dk>
2020-11-11 13:53:47 +0100

    dsad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ControlPanelComponent } from './control-panel/controlpanel.component';
+import {AboutComponent} from "./about/about.component";
+import {PreviewComponent} from './preview/preview.component';
+const appRoutes: Routes = [
+  { path: 'controlpanel', component: ControlPanelComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'login',        component: LoginComponent},
+  { path: 'preview',       component: PreviewComponent},
+  { path: '',   redirectTo: '/login', pathMatch: 'full' },
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
